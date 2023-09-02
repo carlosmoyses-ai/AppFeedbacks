d2fca9c63ef51663800c578a9dd85bfe7dcbcf99
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-02 12:41:10 -0300

    Adicionando na execução do o programa Controllers Swagger e DbContext

--- Feedback.Api/Program.cs
+++ Feedback.Api/Program.cs
@@ -1,6 +1,24 @@
+using Feedback.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddDbContext<ApiDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
