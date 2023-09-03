f92b9d1928ccf3958c7e31298ff3d65bb18a2791
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-03 11:54:25 -0300

    Removendo o FeedbackController.cs e adicionando os Maps no Program.cs

--- Feedback.Api/Program.cs
+++ Feedback.Api/Program.cs
@@ -1,22 +1,84 @@
 using Feedback.Api.Data;
+using Feedback.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddControllers();
+services.AddDbContext<ApiDbContext>();
+
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
-services.AddDbContext<ApiDbContext>();
-
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider;
+    var context = service.GetRequiredService<ApiDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
+app.MapGet("/GetFeedbackList", async (ApiDbContext context) =>
+{
+    var feedbacks = await context.Feedbacks.ToListAsync();
+    await context.SaveChangesAsync();
+    return Results.Ok(feedbacks);
+});
+
+app.MapGet("/GetFeedback/{id}", async (ApiDbContext context, int id) =>
+{
+    var feedback = await context.Feedbacks.FindAsync(id);
+    await context.SaveChangesAsync();
+    return (feedback == null) ? Results.NotFound() : Results.Ok(feedback);
+});
+
+app.MapPost("/PostFeedback", async (ApiDbContext context, FeedbackModel feedback) =>
+{
+    await context.Feedbacks.AddAsync(feedback);
+    await context.SaveChangesAsync();
+    return Results.Created($"/GetFeedback/{feedback.IdFeedback}", feedback);
+});
+
+app.MapPut("/PutFeedback/{id}", async (ApiDbContext context, int id, FeedbackModel feedback) =>
+{
+    var feedbackUpdate = await context.Feedbacks.FindAsync(id);
+    if (feedbackUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    feedbackUpdate.NomeCliente = feedback.NomeCliente;
+    feedbackUpdate.EmailCliente = feedback.EmailCliente;
+    feedbackUpdate.DataFeedback = feedback.DataFeedback;
+    feedbackUpdate.Comentario = feedback.Comentario;
+    feedbackUpdate.Avaliacao = feedback.Avaliacao;
+    await context.SaveChangesAsync();
+    return Results.Ok("Feedback atualizado com sucesso!");
+});
+
+app.MapDelete("/DeleteFeedback/{id}", async (ApiDbContext context, int id) =>
+{
+    var feedback = await context.Feedbacks.FindAsync(id);
+    if (feedback == null)
+    {
+        return Results.NotFound();
+    }
+    context.Feedbacks.Remove(feedback);
+    await context.SaveChangesAsync();
+    return Results.Ok("Feedback removido com sucesso!");
+});
+
+
 
 app.Run();
